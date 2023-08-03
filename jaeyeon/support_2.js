7d0da3783bb81916f38d45faa9dcd0be0a6c38fc
gyo <dldlsry053@naver.com>
2023-08-03 21:23:21 +0900

    <Support> Update Responsive design adjustments for screens under 768px

--- jaeyeon/support_2.js
+++ jaeyeon/support_2.js
@@ -1,5 +1,5 @@
-window.onload=function(){
-    document.getElementById("nav_open_btn").addEventListener("click", function() {
+window.onload = function () {
+    document.getElementById("nav_open_btn").addEventListener("click", function () {
         var nav_open = document.getElementById("navigation_opened");
         var nav_closed = document.getElementById("navigation_closed");
         var main_div = document.getElementsByClassName("main")[0];
@@ -12,7 +12,7 @@ window.onload=function(){
         nav_closed.style.display = "none";
     })
 
-    document.getElementById("nav_close_btn").addEventListener("click", function() {
+    document.getElementById("nav_close_btn").addEventListener("click", function () {
         var nav_open = document.getElementById("navigation_opened");
         var nav_closed = document.getElementById("navigation_closed");
         var main_div = document.getElementsByClassName("main")[0];
@@ -25,70 +25,64 @@ window.onload=function(){
         nav_closed.style.display = "block";
     })
 
-    document.getElementById("instance_toggle").addEventListener("click", function() {
+    document.getElementById("instance_toggle").addEventListener("click", function () {
         var elm = document.getElementById("instance_toggle_contents");
         var status = elm.style.display;
         if (status == "none") {
             elm.style.display = "block";
-        }
-        else {
+        } else {
             elm.style.display = "none";
         }
     })
 
-    document.getElementById("image_toggle").addEventListener("click", function() {
+    document.getElementById("image_toggle").addEventListener("click", function () {
         var elm = document.getElementById("image_toggle_contents");
         var status = elm.style.display;
         if (status == "none") {
             elm.style.display = "block";
-        }
-        else {
+        } else {
             elm.style.display = "none";
         }
     })
 
-    document.getElementById("ebs_toggle").addEventListener("click", function() {
+    document.getElementById("ebs_toggle").addEventListener("click", function () {
         var elm = document.getElementById("ebs_toggle_contents");
         var status = elm.style.display;
         if (status == "none") {
             elm.style.display = "block";
-        }
-        else {
+        } else {
             elm.style.display = "none";
         }
     })
 
-    document.getElementById("net_sec_toggle").addEventListener("click", function() {
+    document.getElementById("net_sec_toggle").addEventListener("click", function () {
         var elm = document.getElementById("net_sec_toggle_contents");
         var status = elm.style.display;
         if (status == "none") {
             elm.style.display = "block";
-        }
-        else {
+        } else {
             elm.style.display = "none";
         }
     })
 
-    document.getElementById("lb_toggle").addEventListener("click", function() {
+    document.getElementById("lb_toggle").addEventListener("click", function () {
         var elm = document.getElementById("lb_toggle_contents");
         var status = elm.style.display;
         if (status == "none") {
             elm.style.display = "block";
-        }
-        else {
+        } else {
             elm.style.display = "none";
         }
     })
 
-    document.getElementById("autoscaling_toggle").addEventListener("click", function() {
+    document.getElementById("autoscaling_toggle").addEventListener("click", function () {
         var elm = document.getElementById("autoscaling_toggle_contents");
         var status = elm.style.display;
         if (status == "none") {
             elm.style.display = "block";
-        }
-        else {
+        } else {
             elm.style.display = "none";
         }
     })
-    
+
 }
